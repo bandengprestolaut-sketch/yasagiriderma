cf61bc5471a318aae9ae2c9bb4405d7084d32af7
sotongbalapan <bandengprestolaut@gmail.com>
2025-09-19 17:42:33 +0700

    Create service-worker.js
    important on progressive web apps for v1.1.1

---
+++ service-worker.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = "yasa-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/image.png"
+];
+
+// Install SW dan cache file
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Aktifasi SW, hapus cache lama
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }))
+    )
+  );
+});
+
+// Fetch resource
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
